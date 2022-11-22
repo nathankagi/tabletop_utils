13d3d4f5506f351e1a586a1ff165a0bde77379cd
Nathan Kagi <npkagi@outlook.com>
2022-11-22 10:28:49 +0800

    card structure and inputs

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,36 @@
+import { FiMenu } from "react-icons/fi";
+
 const Card = ({ name = "Name", ...props }) => {
   return (
-    <div className="card" onClick={CardClick}>
-      <div className="m-2">{name ? name : ""}</div>
+    <div className="card">
+      <div className="flex flex-row m-2">
+        <input className="grow mx-4" defaultValue={name}></input>
+        <div
+          onClick={() => {
+            return console.log("card menu");
+          }}
+        >
+          {<FiMenu />}
+        </div>
+      </div>
       <div className="flex flex-row p-2">
-        <input className="w-10 m-2"></input>
-        <input className="w-10 m-2"></input>
-        <input className="w-10 m-2"></input>
+        <Stat name="Health"></Stat>
+        <Stat name="AC"></Stat>
+        <Stat name="Initiative"></Stat>
       </div>
     </div>
   );
 };
 
-let CardClick = () => {
-  console.log("name");
+let Stat = ({ name }) => {
+  return (
+    <>
+      <div className="flex flex-col m-2 shadow-sm border-2 border-black">
+        <h1 className="h-4 text-center">{name}</h1>
+        <input className="w-12 m-2 text-center"></input>
+      </div>
+    </>
+  );
 };
 
 export default Card;
